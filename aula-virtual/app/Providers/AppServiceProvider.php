f769708d9f338bba42325400b38919b010e2cd06
SmartDataConsulting <soporte@sdc.pe>
2026-08-14 19:34:42 -0500

    Force HTTPS URLs behind production proxy

--- aula-virtual/app/Providers/AppServiceProvider.php
+++ aula-virtual/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 /**
  * Proveedor de servicios base de la aplicacion.
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (str_starts_with((string) config('app.url'), 'https://')) {
+            URL::forceScheme('https');
+        }
+
         Paginator::defaultView('vendor.pagination.smart-data');
         Paginator::defaultSimpleView('vendor.pagination.smart-data-simple');
     }
